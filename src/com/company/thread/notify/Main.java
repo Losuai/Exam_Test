39fd87a6f1434f706cfe9f3b9f5f85195d4b6c30
Losuai <1274042829@qq.com>
2019-08-24 13:28:21 +0800

    添加新题

---
+++ src/com/company/thread/notify/Main.java
@@ -0,0 +1,45 @@
+package com.company.thread.notify;
+
+public class Main {
+    public static void main(String[] args) {
+        Thread[] rs = new Thread[10];
+        for(int i = 0;i < 10;i++) {
+            rs[i] = new Thread(new R(i));
+        }
+        for(Thread r : rs) {
+            r.start();
+        }
+
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        synchronized(obj) {
+            System.out.println("notifyAll");
+            obj.notifyAll();
+        }
+    }
+
+    private static final Object obj = new Object();
+    static class R implements Runnable {
+        int i;
+
+        R(int i) {
+            this.i = i;
+        }
+
+        public void run() {
+            try {
+                synchronized(obj) {
+                    System.out.println("线程->  " + i + " 等待中");
+                    obj.wait();
+                    System.out.println("线程->  " + i + " 在运行了");
+                    Thread.sleep(30000);
+                }
+            } catch(Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
